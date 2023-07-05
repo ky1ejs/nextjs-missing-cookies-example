eac589a12ba4a6756dda9f4ac2a03dcc9173ee90
Kyle Satti <k@kylejs.dev>
2023-07-05 00:38:29 -0400

    fix getAll() cookies

--- app/test-cookies/route.ts
+++ app/test-cookies/route.ts
@@ -2,7 +2,7 @@ import { cookies } from "next/dist/client/components/headers";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const allCookies = cookies().getAll
+  const allCookies = cookies().getAll()
   console.log(`cookies in handler: ${allCookies.length > 0 ? allCookies.toString : 'empty'}`)
   return new NextResponse("Done", {status: 200})
 }
