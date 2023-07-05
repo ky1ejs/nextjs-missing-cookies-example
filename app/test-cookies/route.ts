eebfa0bd47a8a889e77c4b09c42209501d4a956f
Kyle Satti <k@kylejs.dev>
2023-07-05 07:36:48 -0400

    fix toString() usage

--- app/test-cookies/route.ts
+++ app/test-cookies/route.ts
@@ -3,6 +3,6 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   const allCookies = cookies().getAll()
-  console.log(`cookies in handler: ${allCookies.length > 0 ? allCookies.toString : 'empty'}`)
+  console.log(`cookies in handler: ${allCookies.length > 0 ? allCookies.toString() : 'empty'}`)
   return new NextResponse("Done", {status: 200})
 }
